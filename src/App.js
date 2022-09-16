1c7e035dde01fe6653096b3d9df40b54c854a7aa
BuildTools <unconfigured@null.spigotmc.org>
2022-09-16 12:16:01 -0400

    starting a2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome';
 
 function App() {
   return (
+  
     <div className="App">
+     
       <header className="App-header">
+      <Welcome/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
